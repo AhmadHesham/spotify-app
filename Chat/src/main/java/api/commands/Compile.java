3b73eda45d9b1d21d6fe46f268ea8eea5fa0f85d
AhmadHesham <ahmedhsaid@gmail.com>
2021-06-25 03:25:28 +0200

    Class Dynamic Compiling during runtime

--- Chat/src/main/java/api/commands/Compile.java
+++ Chat/src/main/java/api/commands/Compile.java
@@ -1,8 +1,12 @@
 package api.commands;
 
 import java.io.File;
+import java.util.Arrays;
 
+import javax.tools.DiagnosticCollector;
 import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
 
 import api.Command;
@@ -18,11 +22,22 @@ public class Compile extends Command {
     @Override
     public void execute() throws Exception {
         try {
-            File sourceFile = new File("api.commands." + map.get("className"));
+            // File root = new File("/commands");
+            // File sourceFile = new File(root, map.get("className"));
+            // JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+            // compiler.run(null, null, null, sourceFile.getPath());
             JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-            System.out.println();
-            System.out.println("Compiled Successfully!");
-            compiler.run(null, null, null, sourceFile.getPath());
+            DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+            StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+            Iterable<? extends JavaFileObject> compilationUnits = fileManager
+                    .getJavaFileObjectsFromStrings(Arrays.asList("Chat/src/main/java/api/commands/" + map.get("className") + ".java"));
+            String[] arr = {"-cp", "../../../../../target/classes/api/commands"};
+            Iterable<String> options = Arrays.asList();
+            JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, null, options, null, compilationUnits);
+            boolean success = task.call();
+            fileManager.close();
+            System.out.println("Success: " + success);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
